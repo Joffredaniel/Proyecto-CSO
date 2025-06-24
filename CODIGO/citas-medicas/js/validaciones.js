38c59d10f5a523c7c80a91ad5436ec6b12e60940
Joffredaniel <joffredaniel@proton.me>
2025-06-24 15:33:23 -0500

    Delete CODIGO directory

--- CODIGO/citas-medicas/js/validaciones.js
+++
@@ -1,9 +0,0 @@
-function validarFormulario() {
-    const email = document.querySelector("input[name='email']");
-    const password = document.querySelector("input[name='password']");
-    if (email.value === "" || password.value === "") {
-        alert("Todos los campos son obligatorios");
-        return false;
-    }
-    return true;
-}
